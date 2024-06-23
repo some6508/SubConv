619bc970844394a8b1e1f29ac81657b2ce0e61bc
some6508 <58564994+some6508@users.noreply.github.com>
2024-06-23 19:25:29 +0800

    Add files via upload

--- modules/parse.py
+++ modules/parse.py
@@ -12,26 +12,26 @@ import modules.convert.converter as converter
 
 # parse yaml
 async def parseSubs(content):
-    try:
-        proxies =  yaml.safe_dump(
-            {"proxies": yaml.load(content, Loader=yaml.FullLoader).get("proxies")},
-            allow_unicode=True,  # display characters like Chinese
-            sort_keys=False  # keep the original sequence
-        )
-    except:
-        proxies = yaml.safe_dump(
-            {"proxies": await converter.ConvertsV2Ray(content)},
-            allow_unicode=True,  # display characters like Chinese
-            sort_keys=False  # keep the original sequence
-        )
-    return proxies
+	try:
+		proxies = yaml.safe_dump(
+			{"proxies": yaml.load(content, Loader=yaml.FullLoader).get("proxies")},
+			allow_unicode=True,  # display characters like Chinese
+			sort_keys=False  # keep the original sequence
+		)
+	except:
+		proxies = yaml.safe_dump(
+			{"proxies": await converter.ConvertsV2Ray(content)},
+			allow_unicode=True,  # display characters like Chinese
+			sort_keys=False  # keep the original sequence
+		)
+	return proxies
 
 # create a dict containg resions and corresponding proxy group
 async def mkListProxyNames(content: list):
-    providerProxyNames = []
-    if content:
-        for u in content:
-            # preprocess the content
-            contentTmp = re.findall(r"- name: (.+)", u)
-            providerProxyNames.extend(contentTmp)
-    return providerProxyNames
+	providerProxyNames = []
+	if content:
+		for u in content:
+			# preprocess the content
+			contentTmp = re.findall(r"- name: (.+)", u)
+			providerProxyNames.extend(contentTmp)
+	return providerProxyNames
